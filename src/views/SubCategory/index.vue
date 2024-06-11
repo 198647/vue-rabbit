4a67db844565cc9f542dc92d480a79f58520d04b
KeYan-pro <112915389+198647@users.noreply.github.com>
2024-06-11 09:39:44 +0800

    三级面包屑导航

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -1,5 +1,14 @@
 <script setup>
-
+import { getCategoryFilterAPI } from '@/apis/category'
+import { onMounted, ref } from 'vue';
+import { useRoute } from 'vue-router';
+const categoryData = ref({})
+const route = useRoute()
+const getCategoryData = async () => {
+  const res = await getCategoryFilterAPI(route.params.id)
+  categoryData.value = res.result
+}
+onMounted(() => getCategoryData())
 </script>
 
 <template>
@@ -8,7 +17,7 @@
     <div class="bread-container">
       <el-breadcrumb separator=">">
         <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-        <el-breadcrumb-item :to="{ path: '/' }">居家
+        <el-breadcrumb-item :to="{ path: `/category/${categoryData.parentId}` }">{{ categoryData.parentName }}
         </el-breadcrumb-item>
         <el-breadcrumb-item>居家生活用品</el-breadcrumb-item>
       </el-breadcrumb>
@@ -20,7 +29,7 @@
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
       </el-tabs>
       <div class="body">
-         <!-- 商品列表-->
+        <!-- 商品列表-->
       </div>
     </div>
   </div>
