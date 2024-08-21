df6bf62c6e0e895dc64786a704bc11269f775dde
KeYan-pro <112915389+198647@users.noreply.github.com>
2024-08-21 15:06:43 +0800

    本地购物车全选

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -6,7 +6,7 @@ export const useCartstore = defineStore('cart', () => {
     const cartList = ref([])
 
     const addCart = (goods) => {
-        const item = cartList.value.find((item) => { goods.skuId === item.skuId })
+        const item = cartList.value.find((item) => goods.skuId === item.skuId)
         if (item) {
             item.count++
         } else {
@@ -14,23 +14,35 @@ export const useCartstore = defineStore('cart', () => {
         }
     }
     const delcart = (skuId) => {
-        const idx = cartList.value.findIndex((item) => { skuId === item.skuId })
+        const idx = cartList.value.findIndex((item) => skuId === item.skuId)
         cartList.value.splice(idx, 1)
         console.log(cartList.value);
         
     }
 
+    const singleCheck = (skuId,selected)=>{
+        const item = cartList.value.find((item) =>  skuId === item.skuId )
+        item.selected = selected
+
+    }
+
+    const allCheck = (selected)=>{
+        cartList.value.forEach(item => item.selected =selected)
+    }
 
     //计算属性
     const allCount = computed(()=>cartList.value.reduce((a, c)=>a + c.count , 0))
     const allPrice = computed(()=>cartList.value.reduce((a, c)=>a + c.count*c.price , 0))
-    
+    const isAll = computed(()=>cartList.value.every((item)=>item.selected))
     return {
         cartList,
         addCart,
         delcart,
+        singleCheck,
+        allCheck,
         allCount,
-        allPrice
+        allPrice,
+        isAll,
     }
 }, {
     persist: true
